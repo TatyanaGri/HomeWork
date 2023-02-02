e87685bfad188f87121be1861a17d26afd9be760
Tatyana <grigtat13@gmail.com>
2023-02-02 23:09:38 +0300

    HW06

---
+++ HW06/HW06_01/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+вводим через один Enter (разбиваем через Split())
+*/
+
+Console.WriteLine("Введите числа через пробел: ");
+int [] arr = Console.ReadLine().Split(new char []{' '}, StringSplitOptions.RemoveEmptyEntries).Select(number => Convert.ToInt32(number)).ToArray();
+/* 
+создаем массив; 
+split - выводит вводимые элементы в одну строку с нужным разделителем; 
+StringSplitOptions.RemoveEmptyEntries - убирает лишние пробелы (при лишних пробелах выскакивала ошибка ввода)
+Select - последовательно преобразует каждый элемент 
+конвертируем элементы массива в int
+*/
+
+int NulArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+      if(arr[i] > 0) 
+      count = count + 1;
+    }
+    return count;
+}
+
+int result = NulArray(arr);
+Console.WriteLine($"Количество чисел больше 0, введенных пользователем равно: {result}");
---
+++ HW06/HW06_02/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку 
+пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!\n");
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Укажите переменные для двух прямых:");
+Console.WriteLine();
+
+double numberB1 = GetNumber("Введите точку b1: ");
+double numberK1 = GetNumber("Введите точку k1: ");
+Console.WriteLine();
+
+double numberB2 = GetNumber("Введите точку b2: ");
+double numberK2 = GetNumber("Введите точку k2: ");
+Console.WriteLine();
+
+if (numberB1 == numberB2 && numberK1 == numberK2)
+{
+  Console.WriteLine("Прямые совпадают");
+}
+else if ((numberB1 * numberK2 - numberB2 * numberK1) == 0)
+{
+  Console.WriteLine("Прямые паралельны");
+}
+else if ((numberB1 * numberK2 + numberB2 * numberK1) == 0)
+{
+  Console.WriteLine("Прямые перпендикулярны");
+}
+else
+{
+double x = (numberB2-numberB1)/(numberK1-numberK2);
+double y = (numberK1*(numberB2-numberB1))/(numberK1-numberK2)+numberB1;
+
+Console.WriteLine($"Точки пересечения равны ( {x} : {y} )");
+}
